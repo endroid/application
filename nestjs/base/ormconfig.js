377747a75f5012438ef0c1e7380ea4aacbb920b1
Jeroen van den Enden <info@endroid.nl>
2021-12-14 22:42:55 +0100

    Fix path to .env

--- nestjs/base/ormconfig.js
+++ nestjs/base/ormconfig.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: `.env.${process.env.NODE_ENV}`});
+require("dotenv").config({ path: __dirname + `/.env.${process.env.NODE_ENV}`});
 
 module.exports = {
 	type: "postgres",

916463d74f32d65b700ed19474fb0138fa537f28
Jeroen van den Enden <info@endroid.nl>
2023-02-28 09:34:16 +0100

    Layout and component

---
+++ sveltekit/base/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<title>Endroid</title>
+	<meta name="description" content="Endroid.nl" />
+</svelte:head>
+
+<slot />
--- sveltekit/base/src/routes/+page.svelte
+++ sveltekit/base/src/routes/+page.svelte
@@ -1,21 +1,5 @@
-<svelte:head>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-	<title>Endroid</title>
-	<meta name="description" content="SvelteKit" />
-</svelte:head>
-
 <script>
-	let text = 'QR Code contents';
+	import QrCode from "../lib/QrCode.svelte";
 </script>
 
-<table>
-	<tr>
-		<td>
-			<img src="https://localhost:9000/qr-code/default/{text}" alt="QR code" />
-		</td>
-		<td>
-			Generate a QR code
-			<p><input bind:value={text} /></p>
-		</td>
-	</tr>
-</table>
+<QrCode />

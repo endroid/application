d13244cbacb0442b25ecaffa1f7a3492bcdddd43
Jeroen van den Enden <info@endroid.nl>
2024-01-05 10:34:35 +0100

    Override bootstrap

--- symfony/base/assets/app.js
+++ symfony/base/assets/app.js
@@ -1,8 +1,8 @@
 import './bootstrap.js';
 
 // Stylesheet imports
-import './styles/app.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.css'
 
 // Script imports
 import $ from 'jquery';
